55ee54223e5b72b5a900ef9949441d2256066a8f
charleslim88 <charles.lim.3@gmail.com>
2016-10-10 12:52:05 -0400

    Update hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+username = gets.chomp
+puts "Volume Level (1-10)"
+volume_level = gets.chomp
+puts "What is the fur color?"
+fur_color = gets.chomp
+puts "Good candidate for adoption?"
+candidate = gets.chomp
+puts "Estimated Age"
+age = gets.chomp
+if age == nil
+	puts "nil"
+end
