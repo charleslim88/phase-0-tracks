68f67fcb8623a7db5db707198b924037aad0ffe9
charleslim88 <charles.lim.3@gmail.com>
2016-10-10 12:53:39 -0400

    initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+name = gets.chomp
+
+
+puts "How old are you?"
+input = gets.chomp
+
+if input = num
+	
+end
+
+
+
+puts "What year were you born?"
+year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
