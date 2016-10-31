922d295a19dffa7916832b72d79690b2fc429205
charleslim88 <charles.lim.3@gmail.com>
2016-10-31 12:51:14 -0400

    Create algos.js

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+#PSUDOCODE
+
+Function finds longest word or phrase in grouping of strings
+  1.Set longestString variable to empty string
+  2. Loop through the array of words/phrases
+   * if the length of word/phrase is longer than longestString
+     + set longestString to word/phrase
+     
+     longestString = ""
+ function findLongestString(wordsPhrases) {
+ for(var i = 0; i < wordsPhrases.length; i++) {
+    if(wordsPhrases[i].length > longestString.length) {
+      longestString = wordsPhrases[i];
+    };
+   };
+  return longestString
