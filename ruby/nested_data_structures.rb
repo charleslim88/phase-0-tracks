b60375cda5d9ee5661a17b2beda0c1ba06d67578
charleslim88 <charles.lim.3@gmail.com>
2016-11-07 11:46:48 -0500

    Update nested_data_structures.rb

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,11 @@
+family = {
+
+casey = (             # Parent
+    'name' => 'Sue',
+    'age'  => '45');
+peter = (             # Child
+    'name' => 'John',
+    'age'  => '20');
+jimmy = (             # Child
+    'name' => 'Peggy',
+    'age'  => '16');
